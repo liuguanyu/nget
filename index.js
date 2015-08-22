9999082a1792b100943647852cfde3d2bc7781d1
unknown <lgy573@126.com>
2015-08-22 22:37:00 +0800

    增加下载列表，可以在同目录下放置download.txt每行一个地址，以下载

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
+var fs = require("fs");
 var PLine = require("./pline/pline.js");
 
-var pline = new PLine("http://tv.sohu.com/20150817/n419083916.shtml");
-pline.run();
+var downloadList= fs.readFileSync("download.txt").toString().split("\n");
+
+downloadList.filter(function (el){
+	return (el.trim() !== "");
+}).forEach(function (el){
+	var pline = new PLine(el);
+	pline.run();	
+});
