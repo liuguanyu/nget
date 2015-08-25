c57fed210ca716893ae7bba41be12e6b5f71ccb3
梁杰 <lj925184928@gmail.com>
2015-08-25 17:47:41 +0800

    support global nget command & pipe

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
+#!node --harmony
+
 var fs = require("fs");
 var PLine = require("./pline/pline.js");
 
-var downloadList= fs.readFileSync("download.txt").toString().split("\n");
+var args = process.argv.slice(2);
+var promise = new Promise(function(resolve){
+	if (args[0] == "."){
+		fs.readFile("download.txt", function (err, data){
+			if (err) throw err;
+			resolve(data.split("\n"));
+		});
+	}
+	else if (args.length) {
+		resolve(args);
+	}
+	else {
+		var data = "";
+		process.stdin.resume(); // Compatible with "old" stream
+		process.stdin.setEncoding('utf8');
+		process.stdin.on('data', function(chunk) {
+			data += chunk.toString();
+		});
 
-downloadList.filter(function (el){
-	return (el.trim() !== "");
-}).forEach(function (el){
-	var pline = new PLine(el);
-	pline.run();	
+		process.stdin.on('end', function() {
+			resolve(data.split("\n"));
+		});
+	}
 });
+
+promise.then(function (downloadList) {
+	downloadList.filter(function (el){
+		return (el.trim() !== "");
+	}).forEach(function (el){
+		var pline = new PLine(el);
+		pline.run();
+	});
+})
